780a5b35b246d8dffe9246b88b1d8db57f2b4f55
SJaeYub <76236182+SJaeYub@users.noreply.github.com>
2021-07-20 16:49:05 +0900

    baek 1338

--- Main.java
+++ Main.java
@@ -1,71 +1,74 @@
-package baek2667;
+package baek1388;
 
 import java.util.*;
 
 public class Main {
-	static int n;
-	static int [][] map;
+	
+	static int n = 0, total = 0, m = 0;
 	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static ArrayList<Integer> result;
-	static int cnt;
+	static char[][] deco;	
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
+	static boolean flag = false;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		map = new int[n][n];
-		visited = new boolean[n][n];
+		m = sc.nextInt();
+		deco = new char[n][m];		
+		visited = new boolean[n][m];
 		String temp = new String();
-		result = new ArrayList<>();
 		
 		for(int i = 0; i < n; i++) {
 			temp = sc.next();
-			for(int j = 0; j < n; j++) {
-				map[i][j] = temp.charAt(j) - '0';
+			for(int j = 0; j < m; j++) {
+				deco[i][j] = temp.charAt(j);
 			}
 		}
 		
-//		for(int i = 0; i < n; i++) {
-//			for(int j = 0; j < n; j++) {
-//				System.out.println(map[i][j]);
-//			}
-//		}
-	
+		
+		
 		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				if(map[i][j] == 1 && !visited[i][j]) {
-					cnt = 1;
-					dfs(i,j);
-					result.add(cnt);					
+			for(int j = 0; j < m; j++) {
+				if(!visited[i][j]) {
+					System.out.println(i + " " + j);
+					total++;
+					char init_deco = deco[i][j];
+					dfs(i,j, init_deco);
 				}
 			}
 		}
 		
-		Collections.sort(result);
-		System.out.println(result.size());
-		for(int i : result) {
-			System.out.println(i);
-		}
-		
+		System.out.println(total);
+
 	}
 	
-	public static int dfs(int a, int b) {
-		visited[a][b] = true;
-		
-		for(int i = 0; i < 4; i++) {
-			int mx = a + dx[i];
-			int my = b + dy[i];
-			
-			if(mx >= 0 && my >= 0 && mx < n && my < n) {
-				if(map[mx][my] == 1 && !visited[mx][my]) {
-					cnt++;
-					dfs(mx, my);
+	private static int dfs(int a, int b, char d) {
+		if(deco[a][b] == d) {
+			visited[a][b] = true;
+//			System.out.println(a + " " + b);
+			for(int i = 0; i< 4; i++) {
+				int mx = b;
+				int my = a;
+				
+				if(deco[a][b] == '-') {
+					mx = b + dx[i];
+				}
+				if(deco[a][b] == '|') {
+					my = a + dy[i];
+				}
+				if(mx >= 0 && my >= 0 && mx < m && my < n) {
+					if(!visited[my][mx]) {
+						dfs(my, mx, deco[a][b]);
+					}
 				}
 			}
+			return 0;
 		}
-		
-		return cnt;
+		if(deco[a][b] != b) {
+			return 0;
+		}
+		return 0;
 	}
 
 }
